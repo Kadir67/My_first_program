3540eeb9b701d327b371a593c1beb0e18e2c8c6c
Kadir67 <45956749+Kadir67@users.noreply.github.com>
2018-12-18 12:33:23 +0900

    Create first_program.py

---
+++ first_program.py
@@ -0,0 +1,11 @@
+#This program is my first program to say Hello and asks for my name using Python 3.6
+
+print("Hello World")
+print("What is your name?")
+my_name=input()
+print("It is a pleasure to meet you", my_name)
+print("The legth of your name is: ", len(my_name))
+print("What is your age?")
+my_age=input()
+print("You will be", str(int(my_age)+1), "next year.")
+print("Congrat on your first program")
